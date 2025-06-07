6d562d814e31d9df36760cec3cfadf2cee4b0932
Akira <ak13a@tutamail.com>
2025-06-07 18:39:25 -0500

    feat(scan): agregar script checkov
    Agregar script de análisis estático con checkov sobre iac/

---
+++ scripts/scan_checkov.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Ejecuta checkov sobre el directorio iac/ y guarda resultados en reports/checkov.json.
+
+set -e
+
+# Asegura que se trabaje desde el directorio raíz del repositorio
+cd "$(git rev-parse --show-toplevel)"
+
+echo "🔹 Iniciando escaneo de código estático con Checkov en iac/..."
+
+# Crea directorio reports/ en raíz si no existe
+mkdir --parents ../reports
+
+# Ejecuta checkov en iac/ y guardar salida en formato JSON
+checkov --directory iac/ --output json --output-file-path reports/checkov.json
+
+echo "✅ Escaneo con Checkov completado. Resultados guardados en reports/checkov.json"
